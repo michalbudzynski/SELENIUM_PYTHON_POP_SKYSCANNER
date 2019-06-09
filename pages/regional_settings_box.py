8742f49e946d45e5d9e51775e7c2b0338a9a4872
michalbudzynski <budzynski.michal@interia.pl>
2019-06-09 23:33:38 +0200

    Add: click switch to eng function

--- pages/regional_settings_box.py
+++ pages/regional_settings_box.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from pages.base_page import BasePage
+from locators import RegionalSettingsLocators
+
 
 class RegionalSettings(BasePage):
 
     def click_switch_to_eng(self):
-        pass
+        SWITCH_BTN = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.ID, RegionalSettingsLocators.SWITCH_ENG)))
+
+        SWITCH_BTN.click()
 
     def get_language(self):
         pass
