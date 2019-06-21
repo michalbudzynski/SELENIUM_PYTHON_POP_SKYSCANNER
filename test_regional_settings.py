36c33b378be69d7bdcac29b3d99be70c1fa5fdc8
michalbudzynski <budzynski.michal@interia.pl>
2019-06-21 20:22:54 +0200

    edit test

--- test_regional_settings.py
+++ test_regional_settings.py
@@ -14,8 +14,8 @@ class RegionalSettings(unittest.TestCase):
 
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
         self.driver.maximize_window()
--- test_search_flight.py
+++ test_search_flight.py
@@ -11,8 +11,8 @@ class SearchFlight(unittest.TestCase):
 
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
         self.driver.maximize_window()
